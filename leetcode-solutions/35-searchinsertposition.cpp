3d44060d49f4d0ea163da25c590854b27663e73d
Techmeng Aing <aingtechmeng@gmail.com>
2024-09-12 23:14:13 -0400

    did prob #35

---
+++ leetcode-solutions/35-searchinsertposition.cpp
@@ -0,0 +1,28 @@
+##include <vector>
+#include <string>
+#include <algorithm> 
+
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid; // Target found at index mid
+            } else if (nums[mid] < target) {
+                left = mid + 1; // Search in the right half
+            } else {
+                right = mid - 1; // Search in the left half
+            }
+        }
+        
+        // If target is not found, left will be the insert position
+        return left;
+    }
+};
