a0c74d1606be1dba40f94cbcf9d6e4aa10b031f5
Techmeng Aing <125338813+techmengg@users.noreply.github.com>
2025-01-14 20:57:14 -0500

    Create 217_containsduplicate.py

---
+++ leetcode-solutions/217_containsduplicate.py
@@ -0,0 +1,20 @@
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         for i in range(len(nums)):
+#             for j in range(i + 1, len(nums)):
+#                 if nums[i] == nums[j]:
+#                     return True
+#         return False
+
+# Brute force, Exceeded Runtime
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
+        return False
+
+# Accepted.
+        
